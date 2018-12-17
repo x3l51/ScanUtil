579f18f877467c38bbdc8462fcb2a46c7af5e978
x3l51 <31654136+x3l51@users.noreply.github.com>
2018-12-17 15:08:36 +0100

    Update ScanUtil.ps1
    Comments, clearn up, added standard set uof ports to scan for

--- ScanUtil.ps1
+++ ScanUtil.ps1
@@ -1,14 +1,40 @@
 function ScanUtil
-    {$computer=Read-Host "What DOMAIN/ IP should be scanned?"
-        $port=Read-Host "What Port should be scanned for? (seperate by coma)"
-        $adress=[System.Net.Dns]::GetHostAddresses($computer).IPAddressToString
-        $ahost=[System.Net.Dns]::GetHostByAddress($adress).hostname
-        If ($adress -eq $computer)
-            {Write-Host "Domain name is:" $ahost -ForegroundColor Green}
-        else {Write-Host "IP adress is:" $adress -ForegroundColor Green}
-        $port.split(',') | Foreach-Object -Process {
-            If (($a=Test-NetConnection $computer -Port $_ -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true) 
-                {Write-Host $a.Computername"Port" $a.RemotePort -ForegroundColor Green "is open"} 
-            else {Write-Host $a.Computername"Port" $a.RemotePort -ForegroundColor Red "is closed"}
+{
+    ## Get info on host/ port/s
+    $computer = Read-Host "`nEnter IP adress or qualified domain name"
+    $port = Read-Host "`nSeperate multiple ports that should be scanned by`ncoma or press enter to scan a standard set of ports"
+
+    ## Set port variable to set of standard ports when nothing has been entered
+    if (!$port)
+    {
+        $port = "21","22","23","80","443"
+        Write-Host "`nScanning for standard ports."
+    }
+
+    ## Actually get IP of host or domain of IP
+    $adress = [System.Net.Dns]::GetHostAddresses($computer).IPAddressToString
+    $ahost = [System.Net.Dns]::GetHostByAddress($adress).hostname
+
+    ## Decide if IP or Domain has been entered
+    if ($adress -eq $computer) 
+    {
+        Write-Host "`nDomain name is:" $ahost -ForegroundColor Green "`n"
+    } 
+    else 
+    {
+        Write-Host "`nIP adress is:" $adress -ForegroundColor Green "`n"
+    }
+
+    ## Seperate loop for every port entered
+    $port.split(',') | Foreach-Object -Process {
+        if (($a = Test-NetConnection $computer -Port $_ -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true) 
+        {
+            Write-Host $a.Computername"Port" $a.RemotePort -ForegroundColor Green "is open"
+        } 
+        else 
+        {
+            Write-Host $a.Computername"Port" $a.RemotePort -ForegroundColor Red "is closed"
         }
     }
+    Write-Host
+}
